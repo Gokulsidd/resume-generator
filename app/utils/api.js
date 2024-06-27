c49f0e620e31225abebd89b849ef8ede1a3676ec
Gokulsidd <gokulsidharth02@gmail.com>
2024-06-27 23:05:50 +0530

    added backend url

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002', // Your NestJS backend URL
+  baseURL: 'https://resume-generator-backend-hwmi.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
